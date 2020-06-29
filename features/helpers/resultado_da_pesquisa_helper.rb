3fcd6ebec0e086c348a211aa2080cf812ea0ecac
unknown <pedrofcmenegatti@gmail.com>
2020-06-29 11:06:06 -0300

    descricao do modulo

--- features/helpers/resultado_da_pesquisa_helper.rb
+++ features/helpers/resultado_da_pesquisa_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Modulo responsavel por realizar a pesquisa no site
+# Modulo responsavel pelo resultado da pesquisa no site
 module ResultadoDaPesquisaHelper
   class << self
     def resultado_pesquisa
